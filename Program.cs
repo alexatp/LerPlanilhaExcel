b8ac4949ed9749de2a5b833f8ff500bc0e34e9b3
Alexandre Teixeira <alextp@gmail.com>
2023-04-07 00:30:47 -0300

    Criação do program para a leitura do arquivo notas.xlsx

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ClosedXML.Excel;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var xls = new XLWorkbook("notas.xlsx");
+var planilha = xls.Worksheets.First(w => w.Name == "Plan1");
+var totalLinhas = planilha.Rows().Count();
+
+Console.WriteLine("Total de linhas na planilha: " + totalLinhas);
+
+//lendo todas as linhas da planilha
+//os dados começam na linha 2
+for(int l = 2; l <= totalLinhas; l++)
+{
+    Console.WriteLine("Disciplina:" + planilha.Cell($"A{l}").Value.ToString());
+}
